49dc02016273cf00784abcf3d070f4d0b19f68d7
Diego Zubaray <diegozubaray@gmail.com>
2021-03-27 22:20:20 -0300

    Listo para el despliegue

---
+++ src/main/java/com/zubaray/market/ZubarayMarketApplication.java
@@ -0,0 +1,13 @@
+package com.zubaray.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZubarayMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZubarayMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zubaray/market/ZubarayMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zubaray.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZubarayMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
