d6610d8e73df3f40b556e5cba296e066fc214e29
Diego Zubaray <diegozubaray@gmail.com>
2021-04-05 10:07:06 -0300

    Enabling Swagger

--- src/main/java/com/zubaray/market/web/security/SecurityConfig.java
+++ src/main/java/com/zubaray/market/web/security/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-       http.csrf().disable().authorizeRequests().antMatchers("/**/authenticate").permitAll()
+       http.csrf().disable().authorizeRequests().antMatchers("/**/authenticate","/swagger-ui.html", "/swagger-ui/**", "/v2/api-docs/**", "/swagger-resources/**").permitAll()
                .anyRequest().authenticated().and().sessionManagement()
                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
